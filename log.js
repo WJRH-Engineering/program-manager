4928989d44b1f9a9ec1cdb9ae62399fa6f088f54
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2019-10-14 21:05:12 -0400

    more changes

--- log.js
+++ log.js
@@ -7,8 +7,41 @@ const fetch = require('node-fetch')
 const out_pretty = new prettyStream
 out_pretty.pipe(process.stdout)
 
-// const remote_url = 'http://45.55.38.183:4002/log'
 const remote_url = "http://45.55.38.183:4002/log"
+
+// Utility function, returns a promise that 
+// resolves after the given number of seconds
+const pause = function(duration) {
+	return new Promise(function(resolve, reject){
+		setTimeout(resolve, duration * 1000)
+	})
+}
+
+// Sends a post request to the remote_url with the given data
+const write_remote = async function(event, data){
+	const request = fetch(remote_url, {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: "POST",
+			body: JSON.stringify({ 
+				event: event,
+				data: data,
+				sender: "(N)EAL" 
+			})
+		}).catch(err => console.log('heartbeat failed to send'))
+
+	await request
+}
+
+const begin_heartbeat = async function(period){
+	while(true){
+		await pause(period)
+		console.log("sending heartbeat")
+		await write_remote('HEARTBEAT')
+	}
+}
 
 const remote = new stream.Writable({
 	write: async function(chunk, encoding, next){
@@ -44,4 +77,5 @@ const log = bunyan.createLogger({
 	]
 })
 
+begin_heartbeat(5);
 module.exports = log
